7ddcb70f654455a65a65f649d3a278301a202ca7
ppwatchic <ppw4education@gmail.com>
2016-07-14 17:04:36 -0500

    Create coinChangeFastRecursive.java
    use recursive to do the coinChange: 
    1) count = amount / coins[index] : save a lot of time
    2) if(newCount>= total -1)
    break;

---
+++ coinChangeFastRecursive.java
@@ -0,0 +1,30 @@
+public class Solution {
+    int total = Integer.MAX_VALUE;
+    
+    public int coinChange(int[] coins, int amount) {
+        if(amount < 1)
+            return 0;
+        Arrays.sort(coins);
+        count(amount, coins.length - 1, coins, 0);
+        return total == Integer.MAX_VALUE ? -1 : total;  
+	}
+	
+	public void count(int amount, int index, int[] coins, int count) {
+	    if(index < 0 || count >= total -1)
+	        return; 
+	    
+	    int c = amount / coins[index];
+	    for(int i = c; i >=0; i--) {
+	        int rem = amount - i * coins[index];
+	        int newCount = i + count; 
+	        
+	        if(rem>0 && newCount < total)
+	            count(rem, index - 1, coins, newCount);
+	        else if(newCount < total)
+	            total = newCount;
+	            else if(newCount >= total -1)
+	                break;
+	            
+	    }
+	}
+}
