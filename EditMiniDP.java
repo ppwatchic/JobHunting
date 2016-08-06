f1bed90440fa1b9ac5e09ddd9226f4675d9c09a3
ppwatchic <ppw4education@gmail.com>
2016-08-05 23:05:38 -0500

    Create EditMiniDP.java
    dynamic programming to solve editing problem

---
+++ EditMiniDP.java
@@ -0,0 +1,43 @@
+package Algorithms;
+
+// http://www.geeksforgeeks.org/dynamic-programming-set-5-edit-distance/ 
+
+public class EditMiniDP {
+	
+	static int min(int x, int y, int z) {
+		return x < y ? (x < z ? x : z) : (y < z ? y : z);
+	}
+	
+	static int editDistDP(String str1, String str2, int m, int n) {
+		int[][] dp = new int[m + 1][n + 1];
+		
+		for(int i = 0; i <=m; i++) {
+			for(int j = 0; j <=n; j++) {
+				if(i == 0)
+					dp[i][j] = j;
+				else if(j == 0) 
+					dp[i][j] = i;
+				else if(str1.charAt(i - 1) == str2.charAt(j - 1))
+					dp[i][j] = dp[i - 1][j - 1];
+				else 
+					dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j -  1]);
+			}
+		}
+		
+		for(int i = 0; i <= m; i++) {			
+			for(int j = 0; j <= n; j++)
+				System.out.print(dp[i][j] + " ");
+			System.out.println();
+		}
+		return dp[m][n];
+	}
+
+	public static void main(String[] args) {
+		String str1 = "mtuday";
+		String str2 = "saturday";
+		int op = editDistDP(str1, str2, str1.length(), str2.length());
+		System.out.println(op);
+
+	}
+
+}
