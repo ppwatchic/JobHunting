86e0258821aa6abf12ae5a3077dd9e4fa341a6b5
ppwatchic <ppw4education@gmail.com>
2016-12-12 22:18:48 -0600

    Create Test.java

---
+++ DesignPattern/DynamicProxy/Test.java
@@ -0,0 +1,9 @@
+package com.concretepage.proxy;
+
+public class Test {
+  public static void main(String[] args) {
+    Task task = (Task)ProxyFactory.newInstance(new TaskImpl());
+    task.setData("Test");
+    System.out.println(task.getCalData(5));
+  }
+}  
