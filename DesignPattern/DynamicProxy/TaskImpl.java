5d6735d0a60df7584aa729910d592207f40fe2e7
ppwatchic <ppw4education@gmail.com>
2016-12-12 22:07:10 -0600

    Create TaskImpl.java

---
+++ DesignPattern/DynamicProxy/TaskImpl.java
@@ -0,0 +1,14 @@
+package com.concretepage.proxy;
+
+public class TaskImpl implements Task {
+
+  @Override 
+  public void setData(String data) {
+    System.out.println(data + " Data is saved.");
+  }
+  
+  @Override 
+  public int getCalData(int x) {
+    return x * 10;
+  }
+}  
